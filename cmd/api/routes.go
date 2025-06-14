dcc90210c1465cb7860674262b38c3153c5354ca
gargalloeric <erickmontes555@gmail.com>
2025-06-14 17:01:19 +0200

    feat(api): add routes

---
+++ cmd/api/routes.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func (app *application) routes() http.Handler {
+	router := chi.NewRouter()
+
+	router.Use(app.recoverPanic)
+
+	router.NotFound(app.notFoundResponse)
+	router.MethodNotAllowed(app.methodNotAllowedResponse)
+
+	router.Get("/v1/healthcheck", app.healthcheckHandler)
+
+	router.Route("/v1/movies", func(r chi.Router) {
+		r.Post("/", app.createMovieHandler)
+		r.Get("/{id}", app.showMovieHandler)
+	})
+
+	return router
+}
