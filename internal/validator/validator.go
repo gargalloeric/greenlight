7b54224eb6fd6b70cb998397a72ba29fa39921ff
gargalloeric <erickmontes555@gmail.com>
2025-06-15 19:16:50 +0200

    feat(validator): add validator struct and helper functions

---
+++ internal/validator/validator.go
@@ -0,0 +1,59 @@
+package validator
+
+import (
+	"regexp"
+	"slices"
+)
+
+var (
+	// Taken from https://html.spec.whatwg.org/#valid-e-mail-address
+	EmailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+)
+
+type Validator struct {
+	Errors map[string]string
+}
+
+func New() *Validator {
+	return &Validator{
+		Errors: make(map[string]string),
+	}
+}
+
+func (v *Validator) Valid() bool {
+	return len(v.Errors) == 0
+}
+
+func (v *Validator) AddError(key, message string) {
+	if _, exists := v.Errors[key]; !exists {
+		v.Errors[key] = message
+	}
+}
+
+func (v *Validator) Check(ok bool, key, message string) {
+	if !ok {
+		v.Errors[key] = message
+	}
+}
+
+// Generic function which returns true if a specific value is in a list of permitted
+// values.
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
+	return slices.Contains(permittedValues, value)
+}
+
+// Matches returns true if a string value matches a specific regexp pattern.
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+
+// Generic function which returns true if all values in a slice are unique.
+func Unique[T comparable](values []T) bool {
+	uniqueValues := make(map[T]bool)
+
+	for _, v := range values {
+		uniqueValues[v] = true
+	}
+
+	return len(uniqueValues) == len(values)
+}
