76375c7791bc80417a3d4ee4dfb76a66edcdf9df
gargalloeric <erickmontes555@gmail.com>
2025-06-20 13:55:07 +0200

    fix(db): change min allowed movie year

--- migrations/00002_add_movies_check_constraints.sql
+++ migrations/00002_add_movies_check_constraints.sql
@@ -2,7 +2,7 @@
 -- +goose StatementBegin
 ALTER TABLE movies ADD CONSTRAINT movies_runtime_check CHECK (runtime >= 0);
 
-ALTER TABLE movies ADD CONSTRAINT movies_year_check CHECK (year BETWEEN 1988 AND date_part('year', now()));
+ALTER TABLE movies ADD CONSTRAINT movies_year_check CHECK (year BETWEEN 1888 AND date_part('year', now()));
 
 ALTER TABLE movies ADD CONSTRAINT genres_length_check CHECK (array_length(genres, 1) BETWEEN 1 AND 5);
 -- +goose StatementEnd
