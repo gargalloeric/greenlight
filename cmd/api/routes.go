4d62f3364f435e15392b99f1d35df11ef35eab8e
gargalloeric <erickmontes555@gmail.com>
2025-06-21 13:18:48 +0200

    feat(api): add delte and update handlers

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -19,6 +19,8 @@ func (app *application) routes() http.Handler {
 	router.Route("/v1/movies", func(r chi.Router) {
 		r.Post("/", app.createMovieHandler)
 		r.Get("/{id}", app.showMovieHandler)
+		r.Patch("/{id}", app.updateMovieHandler)
+		r.Delete("/{id}", app.deleteMovieHandler)
 	})
 
 	return router
